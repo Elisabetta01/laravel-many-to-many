a0b2adb39f536f256e199a0fc582e74ccaf63b87
Elisabetta01 <scagione.elisabetta@gmail.com>
2023-06-28 17:00:54 +0200

    primo commit

---
+++ resources/views/admin/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('title')
+
+    Laravel-auth | Projects Show
+
+@endsection
+
+@section('content')
+    <div class="container">
+        <h2>Singolo Progetto: {{ $project->id }}</h2>
+
+        <div class="mt-4">
+             <h3>{{ $project->title }}</h3>
+             <p>{{ $project->description }}</p>
+             <img src="{{ asset('storage/' . $project->img) }}" alt="" class="img-fluid">
+
+             <div>
+                Categoria: {{ $project->category->name }}
+             </div>
+        </div>
+
+     </div>
+@endsection
+
